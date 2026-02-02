6f3950e110a41d34179adf629b41f8c6a4fda776
Piers Williams <piers.williams@red-gate.com>
2026-02-02 09:04:02 +0000

    Add database migration for OSM OAuth support
    - Rename OsmApiToken to OsmRefreshToken
    - Add OsmAccessToken field
    - Update database schema for OAuth 2.0 token storage

---
+++ BookingsAssistant.Api/Migrations/20260202090332_AddOsmOAuthSupport.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingsAssistant.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOsmOAuthSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OsmApiToken",
+                table: "ApplicationUsers",
+                newName: "OsmRefreshToken");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OsmAccessToken",
+                table: "ApplicationUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OsmAccessToken",
+                table: "ApplicationUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "OsmRefreshToken",
+                table: "ApplicationUsers",
+                newName: "OsmApiToken");
+        }
+    }
+}
